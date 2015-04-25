3a7a9f158a29bb5f859ed9934c098f0fbbe5398b
overtrue <anzhengchao@gmail.com>
2015-04-25 23:16:24 +0800

    更新配置文件格式与添加外观

--- src/config.php
+++ src/config.php
@@ -1,7 +1,8 @@
 <?php
 return [
-    'appId'          => 'Your app id', // 必填
-    'secret'         => 'Your secret', // 必填
-    'token'          => 'Your token',  // 必填
-    'encodingAESKey' => 'Your encoding AES Key' // 加密模式需要，其它模式不需要
+    'use_alias'    => false,
+    'app_id'       => 'YourAppId', // 必填
+    'secret'       => 'YourSecret', // 必填
+    'token'        => 'YourToken',  // 必填
+    'encoding_key' => 'YourEncodingAESKey' // 加密模式需要，其它模式不需要
 ];
