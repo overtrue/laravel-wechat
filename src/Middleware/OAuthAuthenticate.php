560b6bfd8a82969075a105f39c81179b6bfe6fad
overtrue <anzhengchao@gmail.com>
2016-03-14 16:39:15 +0800

    Bugfix for redirect url.

--- src/Middleware/OAuthAuthenticate.php
+++ src/Middleware/OAuthAuthenticate.php
@@ -25,12 +25,26 @@ class OAuthAuthenticate
             if ($request->has('state') && $request->has('code')) {
                 session(['wechat.oauth_user' => $wechat->oauth->user()]);
 
-                return redirect()->to(url($request->url().'?'.array_except($request->query(), ['code', 'state'])));
+                return redirect()->to($this->getTargetUrl($request));
             }
 
             return $wechat->oauth->redirect($request->fullUrl());
         }
 
         return $next($request);
+    }
+
+    /**
+     * Build the target business url.
+     *
+     * @param Request $request
+     *
+     * @return string
+     */
+    public function getTargetUrl($request)
+    {
+        $queries = array_except($request->query(), ['code', 'state']);
+
+        return $request->url().(empty($queries) ? '' : '?'.http_build_query($queries));
     }
 }
