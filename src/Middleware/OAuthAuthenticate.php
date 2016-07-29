2610766dfce328b4d3d8027ccfa98a87d3507e81
overtrue <anzhengchao@gmail.com>
2016-07-29 08:31:39 -0400

    Applied fixes from StyleCI

--- src/Middleware/OAuthAuthenticate.php
+++ src/Middleware/OAuthAuthenticate.php
@@ -7,16 +7,17 @@ use Event;
 use Overtrue\LaravelWechat\Events\WeChatUserAuthorized;
 
 /**
- * Class OAuthAuthenticate
+ * Class OAuthAuthenticate.
  */
 class OAuthAuthenticate
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     * @param string|null              $guard
+     *
      * @return mixed
      */
     public function handle($request, Closure $next, $guard = null)
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,17 +2,17 @@
 
 namespace Overtrue\LaravelWechat;
 
-use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
 use EasyWeChat\Foundation\Application;
 use Illuminate\Foundation\Application as LaravelApplication;
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
 use Laravel\Lumen\Application as LumenApplication;
 
 class ServiceProvider extends LaravelServiceProvider
 {
     /**
-     * 延迟加载
+     * 延迟加载.
      *
-     * @var boolean
+     * @var bool
      */
     protected $defer = true;
 
@@ -33,11 +33,11 @@ class ServiceProvider extends LaravelServiceProvider
      */
     protected function setupConfig()
     {
-        $source = realpath(__DIR__ . '/config.php');
-        
+        $source = realpath(__DIR__.'/config.php');
+
         if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
             $this->publishes([
-                $source => config_path('wechat.php')
+                $source => config_path('wechat.php'),
             ]);
         } elseif ($this->app instanceof LumenApplication) {
             $this->app->configure('wechat');
@@ -52,7 +52,7 @@ class ServiceProvider extends LaravelServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(['EasyWeChat\\Foundation\\Application' => 'wechat'], function($app){
+        $this->app->singleton(['EasyWeChat\\Foundation\\Application' => 'wechat'], function ($app) {
             $app = new Application(config('wechat'));
 
             if (config('wechat.use_laravel_cache')) {
