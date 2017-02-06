695f133fb696843048f98772fc5ba2a5ae863fcf
mingyoung <mingyoungcheung@gmail.com>
2017-02-06 10:11:52 +0800

    open-platform support

---
+++ src/Events/OpenPlatform/UpdateAuthorized.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Overtrue\LaravelWechat\Events\OpenPlatform;
+
+use EasyWeChat\Support\Collection;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateAuthorized
+{
+    use SerializesModels;
+
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param \EasyWeChat\Support\Collection $event
+     */
+    public function __construct(Collection $event)
+    {
+        $this->message = $event;
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return [];
+    }
+}
