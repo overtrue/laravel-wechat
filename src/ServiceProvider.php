14b1f55add5fc83aae06a30c9b241ef41859fbbc
BranchZero Sun <branchzero@gmail.com>
2017-09-28 11:23:38 +0800

    Fix Lumen 5.5 Compatible Problem

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -101,7 +101,7 @@ class ServiceProvider extends LaravelServiceProvider
      */
     protected function registerRoutes()
     {
-        $router = $this->app instanceof LaravelApplication ? $this->app['router'] : $this->app;
+        $router = $this->app instanceof LaravelApplication || isset($this->app['router']) ? $this->app['router'] : $this->app;
 
         // Register open-platform routes...
         $router->group(config('wechat.route.open_platform.attributes', []), function ($router) {
