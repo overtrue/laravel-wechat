4bcf1a68943b138210970bd290c1c96bd210343b
HanSon <h@hanc.cc>
2017-10-26 15:26:14 +0800

    Update config.php

--- src/config.php
+++ src/config.php
@@ -115,7 +115,6 @@ return [
     // 'payment' => [
     //     'sandbox'            => env('WECHAT_PAYMENT_SANDBOX', false),
     //     'app_id'             => env('WECHAT_PAYMENT_APPID', ''),
-    //     'secret'             => env('WECHAT_PAYMENT_SECRET', ''),
     //     'mch_id'             => env('WECHAT_PAYMENT_MCh_ID', 'your-mch-id'),
     //     'key'                => env('WECHAT_PAYMENT_KEY', 'key-for-signature'),
     //     'cert_path'          => env('WECHAT_PAYMENT_CERT_PATH', 'path/to/cert/apiclient_cert.pem'),    // XXX: 绝对路径！！！！
