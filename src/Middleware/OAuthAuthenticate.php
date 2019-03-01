64d424a9cb8ce82321f1d6e5a053c53e82afcb32
杨进春 <imjinchun@gmail.com>
2019-03-01 18:43:18 +0800

    5.8 兼容问题。 (#301)
    * laravel 5.4 起弃用 Event 的 fire 方法。5.8 起正式移除该方法。
    
    * laravel 5.8 起弃用 str_* 和 arr_* 助手方法，5.9 起移除。

--- src/Middleware/OAuthAuthenticate.php
+++ src/Middleware/OAuthAuthenticate.php
@@ -12,8 +12,9 @@
 namespace Overtrue\LaravelWeChat\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Event;
 use http\Env\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Overtrue\LaravelWeChat\Events\WeChatUserAuthorized;
 
 /**
@@ -33,7 +34,7 @@ class OAuthAuthenticate
     public function handle($request, Closure $next, $account = 'default', $scopes = null)
     {
         // $account 与 $scopes 写反的情况
-        if (is_array($scopes) || (\is_string($account) && str_is('snsapi_*', $account))) {
+        if (is_array($scopes) || (\is_string($account) && Str::is('snsapi_*', $account))) {
             list($account, $scopes) = [$scopes, $account];
             $account || $account = 'default';
         }
@@ -42,7 +43,7 @@ class OAuthAuthenticate
         $sessionKey = \sprintf('wechat.oauth_user.%s', $account);
         $config = config(\sprintf('wechat.official_account.%s', $account), []);
         $officialAccount = app(\sprintf('wechat.official_account.%s', $account));
-        $scopes = $scopes ?: array_get($config, 'oauth.scopes', ['snsapi_base']);
+        $scopes = $scopes ?: Arr::get($config, 'oauth.scopes', ['snsapi_base']);
 
         if (is_string($scopes)) {
             $scopes = array_map('trim', explode(',', $scopes));
@@ -55,7 +56,7 @@ class OAuthAuthenticate
                 session([$sessionKey => $officialAccount->oauth->user() ?? []]);
                 $isNewSession = true;
 
-                Event::fire(new WeChatUserAuthorized(session($sessionKey), $isNewSession, $account));
+                event(new WeChatUserAuthorized(session($sessionKey), $isNewSession, $account));
 
                 return redirect()->to($this->getTargetUrl($request));
             }
@@ -65,7 +66,7 @@ class OAuthAuthenticate
             return $officialAccount->oauth->scopes($scopes)->redirect($request->fullUrl());
         }
 
-        Event::fire(new WeChatUserAuthorized(session($sessionKey), $isNewSession, $account));
+        event(new WeChatUserAuthorized(session($sessionKey), $isNewSession, $account));
 
         return $next($request);
     }
@@ -79,7 +80,7 @@ class OAuthAuthenticate
      */
     protected function getTargetUrl($request)
     {
-        $queries = array_except($request->query(), ['code', 'state']);
+        $queries = Arr::except($request->query(), ['code', 'state']);
 
         return $request->url().(empty($queries) ? '' : '?'.http_build_query($queries));
     }
