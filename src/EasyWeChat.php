d413564fb507bda7563cc0855f26e05bc8b9be93
overtrue <anzhengchao@gmail.com>
2022-03-02 17:30:57 +0800

    修复名称错误

--- src/EasyWeChat.php
+++ src/EasyWeChat.php
@@ -8,32 +8,32 @@ class EasyWeChat extends Facade
 {
     public static function getFacadeAccessor(): string
     {
-        return 'wechat.official_account';
+        return 'easywechat.official_account';
     }
 
     public static function officialAccount(string $name = 'default'): \EasyWeChat\OfficialAccount\Application
     {
-        return app('wechat.official_account.'.$name);
+        return app('easywechat.official_account.'.$name);
     }
 
     public static function work(string $name = 'default'): \EasyWeChat\Work\Application
     {
-        return app('wechat.work.'.$name);
+        return app('easywechat.work.'.$name);
     }
 
 
     public static function payment(string $name = 'default'): \EasyWeChat\Pay\Application
     {
-        return app('wechat.payment.'.$name);
+        return app('easywechat.payment.'.$name);
     }
 
     public static function miniApp(string $name = 'default'): \EasyWeChat\MiniApp\Application
     {
-        return app('wechat.mini_program.'.$name);
+        return app('easywechat.mini_program.'.$name);
     }
 
     public static function openPlatform(string $name = 'default'): \EasyWeChat\OpenPlatform\Application
     {
-        return app('wechat.open_platform.'.$name);
+        return app('easywechat.open_platform.'.$name);
     }
 }
